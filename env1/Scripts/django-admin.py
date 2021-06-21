1f4494df312ccf971fde2d8a4d7f92a8ff97b8c8
19045168 <19045168@DESKTOP-S91NBE6>
2021-06-21 12:55:03 +0800

    Changed Virtual Environment to 3.7 + installed PYODBC for interfacing with Azure database.

---
+++ env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\19045168\Source\Repos\19017515-SamanthaTeoXingYee\C300-FYP\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
